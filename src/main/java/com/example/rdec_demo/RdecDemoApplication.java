09920ada16928b23cb1ed3c0515f6dd91c0cbb40
Siddharth <siddharth.sharma212004@gmail.com>
2024-12-03 15:17:11 +0530

    project read

---
+++ src/main/java/com/example/rdec_demo/RdecDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.rdec_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdecDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdecDemoApplication.class, args);
+
+		System.out.println("App Started");
+	}
+
+}
---
+++ src/test/java/com/example/rdec_demo/RdecDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rdec_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdecDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
